84808388ca866fa9983d95f3a6e12aa8da7a6bde
xrenjie <35645792+xrenjie@users.noreply.github.com>
2022-03-02 12:53:21 -0700

    Update Home.js

--- twit-thread/src/components/Home.js
+++ twit-thread/src/components/Home.js
@@ -1,15 +1,13 @@
 import React from "react";
 import SearchBar from "./SearchBar";
-import Loading from "./Loading";
 const Home = ({ setTweetId }) => {
   return (
-    <Loading />
-    // <div className="w-full flex items-center pt-40 flex-col">
-    //   <h1 className="text-3xl text-center">
-    //     Read Twitter threads in a better way.
-    //   </h1>
-    //   <SearchBar />
-    // </div>
+    <div className="w-full flex items-center pt-40 flex-col">
+      <h1 className="text-3xl text-center">
+        Read Twitter threads in a better way.
+      </h1>
+      <SearchBar />
+    </div>
   );
 };
 
