1dd7578a72f49512a3e7f6ff9728dce352a3184d
xrenjie <35645792+xrenjie@users.noreply.github.com>
2022-03-02 12:15:27 -0700

    update link to tweet

--- twit-thread/src/components/Tweet.js
+++ twit-thread/src/components/Tweet.js
@@ -5,14 +5,22 @@ import TweetBody from "./TweetBody";
 const Tweet = ({ tweets, tweet, user, users, prevColor }) => {
   return (
     <div
-      className={`p-2 pb-0 ml-3 border-l-2 border-t-2 ${
-        tweet.root ? "border-l-4 border-gray-400" : ""
+      className={`px-2 pt-2 mt-2 rounded ml-2 border-l-2 border-t-2 ${
+        tweet.root ? "border-l-[3] border-gray-400" : ""
       }`}
     >
       <UserInfo user={user} tweet={tweet} />
       <TweetBody tweet={tweet} />
       <div className="text-xs">
-        {new Date(tweet.created_at).toLocaleString() + " "}
+        {user ? (
+          <a
+            href={`https://www.twitter.com/${user.username}/status/${tweet.id}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            {new Date(tweet.created_at).toLocaleString() + " "}
+          </a>
+        ) : null}
       </div>
       {tweets[tweet.id]
         ? tweets[tweet.id].map((reply) => {
